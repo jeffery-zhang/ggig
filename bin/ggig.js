57d12b3f40f193fe06dfae04013c547b45c73404
zhangjingwz@htgd.com.cn <jefferyzhang1013@gmail.com>
2023-04-07 00:30:44 +0800

    bugfix

--- bin/ggig.js
+++ bin/ggig.js
@@ -1,25 +1,34 @@
 #!/usr/bin/env node
 
+import path from 'path'
+import chalk from 'chalk'
 import { globSync } from 'glob'
 import { program } from 'commander'
 
+const dirname = path.resolve()
 const paths = globSync('./commands/*.js')
 
 const setCommand = async () => {
   const promises = []
   paths.map(cmdPath => {
+    console.log(cmdPath)
     promises.push(
       new Promise(async resolve => {
-        const { default: obj } = await import(`../${cmdPath}`)
-        const cmd = program
-          .command(obj.command)
-          .description(obj.description)
-          .action(obj.action)
-
-        obj.options && obj.options.map(opt => {
-          cmd.option(...opt)
-        })
-        resolve()
+        try {
+          const { default: obj } = await import(`file://${path.join(dirname, cmdPath)}`)
+          const cmd = program
+            .command(obj.command)
+            .description(obj.description)
+            .action(obj.action)
+  
+          obj.options && obj.options.map(opt => {
+            cmd.option(...opt)
+          })
+          resolve()
+        } catch(err) {
+          console.error(chalk.bgRed(err))
+          process.exit(1)
+        }
       })
     )
   })
